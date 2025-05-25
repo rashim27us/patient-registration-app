050b74513e06c9236b0d1d7583fcdd8328df2cd4
rashim27us <rashimkashyap623@gmail.com>
2025-05-25 13:50:44 +0530

    Add Header component for displaying the application title and user controls

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="header-content">
+        <h1>Patient Registration System</h1>
+        <div className="user-controls">
+          <span className="user-name">Demo User</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
